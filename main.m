4cafbfc54533204e4aaab67d3510a1b72b156299
Bartosz <b.lodek@softwarepartner.pl>
2020-10-16 11:31:01 +0200

    estimate parameters

--- main.m
+++ main.m
@@ -2,7 +2,7 @@
 clear all;
 close all;
 
-N = 1000; % Number of samples
+N = 10000; % Number of samples
 U = rand(1,N); % Random input - u[0, 1]
 K_vec = [1, 2, 3, 4, 5]; % Vector of multipliers of base function
                          % Must be size [1 x K] because of dot product
@@ -25,16 +25,16 @@ Vk = zeros(1,N);
 
 % Convolution calculation
 for i=1:N
-    sumi = 0;
+    phi = zeros(1, S);
     for j=1:S
         if i - j + 1 <= 0    % if idx less t
             skl_mi = 0;
         else
             skl_mi = Wk(i-j+1);
         end  
-        sumi = sumi + (GammaV(j)*skl_mi);
+        phi(j) = skl_mi;
     end
-   Vk(i) = sumi;
+   Vk(i) = phi*GammaV;
 end
 
 % ---<<< Noise 
@@ -45,5 +45,25 @@ Zk = Zk_variance.*randn(1,N) + Zk_mean;
 Yk = Vk + Zk;
 
 % parametric identification
-c = (U*U.')^-1*U*Yk.';
-o = (U*U.')^-1*U*Wk.';
+Phi = [];
+for i = 1:N
+    phi = [];
+    for j = 0:S-1
+        if i - j > 0
+            h = hermite(U(i-j), K - 1);
+        else
+            h = zeros(K, 1);
+        end
+        phi=[phi, h.'];
+    end
+   Phi = [Phi, phi.'];
+end
+teta = (Phi*Phi.')^-1*Phi*Yk.';
+M = reshape(teta, [K, S]);
+[U, S, V] = svd(M);
+GammaV.';
+K_vec;
+p1=U(:,1);
+q1=V(1,:);
+constK = p1./K_vec.'
+constG = q1./GammaV.'
